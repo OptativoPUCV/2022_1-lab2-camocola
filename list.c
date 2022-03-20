ebd7bba25f1ce2c601b8c912ecb18616a755ce58
camocola <>
2022-03-20 21:34:53 +0000

    new try

--- list.c
+++ list.c
@@ -44,8 +44,8 @@ void * firstList(List * list) {
 
 void * nextList(List * list) {
     if (list->head->next == NULL) return NULL;
-    if (list->current->next == NULL) return NULL;
     list->current = list->head->next;
+    if (list->current->data == NULL) return NULL;
     return list->current->data;
 }
 
