9f8c29ae9b8c7f19d9a97bf5592c3d6cdc5115b3
camocola <>
2022-03-20 22:12:16 +0000

    new try

--- list.c
+++ list.c
@@ -57,7 +57,7 @@ void * lastList(List * list) {
 }
 
 void * prevList(List * list) {
-    if (list->current == list->head) return NULL;
+    //if (list->current == list->head) return NULL;
     if (list->head->prev == NULL) return NULL;
     if (list->current == NULL) return NULL;
     list->current = list->current->prev;
