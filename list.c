7591e165bbc2a773ef09a6731ec2a5491e1a003a
camocola <>
2022-03-21 02:56:11 +0000

    new try

--- list.c
+++ list.c
@@ -111,7 +111,7 @@ void * popBack(List * list) {
 void * popCurrent(List * list) {
     Node* new = list->head;
     if (list->current == list->head){
-      new->next = list->head;
+      //new->next = list->head;
       list->head = list->current->next;
       free(list->current);
       list->current = list->head;
