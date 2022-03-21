f80fd38c66329aa7182360a856bf159d0388acc4
camocola <>
2022-03-21 00:10:36 +0000

    new try

--- list.c
+++ list.c
@@ -58,13 +58,14 @@ void * lastList(List * list) {
 
 void * prevList(List * list) {
     if (list->current == list->head) return NULL;
+    if (current == NULL) return null;
     if (list->current->prev == NULL) return NULL;
     list->current = list->current->prev;
     return list->current->data;
 }
 
 void pushFront(List * list, void * data) {
-  
+    
 }
 
 void pushBack(List * list, void * data) {
