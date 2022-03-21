ffe91ec51504de25a9426c1a9a20de8d61c7d21f
camocola <>
2022-03-21 02:02:38 +0000

    new try

--- list.c
+++ list.c
@@ -85,10 +85,11 @@ void pushBack(List * list, void * data) {
 
 void pushCurrent(List * list, void * data) {
     Node* new = createNode(data);
+    list->current->next->prev = new;
     new->data = data;
     new->prev = list->current;
     new->next = list->current->next;
-    list->current->next->prev = new;
+    //list->current->next->prev = new;
     list->current->next = new;
 }
 
